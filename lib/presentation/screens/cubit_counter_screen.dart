46230759cb6710c507dc5d0befeedd676a057761
Ezequiel <ingepasillasg@gmail.com>
2026-02-10 08:34:19 -0600

    feat: acceder a los metodos de counter

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Widget para utilizar Bloc
+    return BlocProvider(
+      // Definir cubit
+      create: (_) => CounterCubit(),
+      // Solo este widget tiene acceso al CounterCuibir
+      child: _CubitCounterView(),
+    );
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  const _CubitCounterView();
+
+  @override
+  Widget build(BuildContext context) {
+    final counterState = context.watch<CounterCubit>().state;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cubit Counter'),
+        title: Text('Cubit Counter: ${counterState.transactionCount}'),
         actions: [
           IconButton(
-            onPressed: () {},
+            // Una manera de acceder a los metodos
+            onPressed: () => context.read<CounterCubit>().reset(),
             icon: const Icon(Icons.refresh_outlined),
           ),
         ],
@@ -24,26 +44,34 @@ class CubitCounterScreen extends StatelessWidget {
 class _FloatingActionButton extends StatelessWidget {
   const _FloatingActionButton();
 
+  // Tercera manera de acceder al metodo
+  void increaseCounterBy(BuildContext context, int value) {
+    context.read<CounterCubit>().increaseBy(value);
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Segunda manera de acceder a los metodos
+    final counterState = context.read<CounterCubit>();
+
     return Column(
       spacing: 15,
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         FloatingActionButton(
-          onPressed: () {},
+          onPressed: () => counterState.increaseBy(3),
           heroTag: '1',
           child: const Text('+3'),
         ),
 
         FloatingActionButton(
-          onPressed: () {},
+          onPressed: () => increaseCounterBy(context, 2),
           heroTag: '2',
           child: const Text('+2'),
         ),
 
         FloatingActionButton(
-          onPressed: () {},
+          onPressed: () => increaseCounterBy(context, 1),
           heroTag: '3',
           child: const Text('+1'),
         ),
@@ -57,6 +85,13 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text('Counter value: xxxx'));
+    return Center(
+      child: BlocBuilder<CounterCubit, CounterState>(
+        buildWhen: (previous, current) => current.counter != previous.counter,
+        builder: (context, state) {
+          return Text('Counter value: ${state.counter}');
+        },
+      ),
+    );
   }
 }
